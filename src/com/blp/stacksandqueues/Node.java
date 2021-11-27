a01c8098106924fa39f226f3f545c4c8284cb65e
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-11-28 00:03:47 +0530

    Added: Dequeue elements file is added

--- src/com/blp/stacksandqueues/Node.java
+++ src/com/blp/stacksandqueues/Node.java
@@ -3,4 +3,8 @@ package com.blp.stacksandqueues;
 public class Node {
     int data;       // integer data
     Node next;      // pointer to the next node
+
+    public Node(int data) {
+        this.data=data;
+    }
 }
